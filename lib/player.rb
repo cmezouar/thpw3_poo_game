4c5b0536248f9ebdf66beca457c068f9e5be6c3d
cmezouar <chloe-mez@hotmail.fr>
2019-10-16 01:36:51 +0200

    class HumanPlayer

--- lib/player.rb
+++ lib/player.rb
@@ -33,3 +33,61 @@ class Player
     end
 
 end
+
+class HumanPlayer < Player
+    
+    attr_accessor :weapon_level
+
+    def initialize(player_name)
+        @name = player_name
+        @life_points = 100
+        @weapon_level = 1
+
+    end
+
+    def show_state
+        puts "#{@name} a #{@life_points} points de vie et une arme de niveau #{@weapon_level}."
+    end
+
+    def compute_damage
+        return rand(1..6) * @weapon_level
+    end
+
+    def search_weapon
+        new_weapon_level = rand(1..6)
+        puts "Tu as trouvé une arme de niveau #{new_weapon_level}."
+        if new_weapon_level > @weapon_level
+            @weapon_level = new_weapon_level
+            puts "Youhou! Elle est meilleure que ton arme actuelle : tu la prends."            
+        else
+            puts "M@*#$... elle n'est pas mieux que ton arme actuelle..."
+        end
+    end
+
+    def search_health_pack
+        dice = rand(1..6)
+        if dice == 1
+            puts "Tu n'as rien trouvé..."
+        elsif dice >= 2 && dice <= 5
+            puts "Bravo, tu as trouvé un pack de +50 points de vie!"
+            diff = 100 - @life_points
+            if diff >= 50
+                @life_points = @life_points + 50     
+            else
+                @life_points = @life_points + diff
+            end
+        else
+            puts "Wow, tu as trouvé un pack de +80 points de vie!"
+            diff = 100 - @life_points
+            if diff >= 80
+                @life_points = @life_points + 80     
+            else
+                @life_points = @life_points + diff
+            end
+        end
+    end
+
+end
+
+human = HumanPlayer.new ("Chloé")
+binding.pry
